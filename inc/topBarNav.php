56301ee0fbd9e5fb1d7beab90baf1e3a3ae2e555
marsheloow <yeshintarangian026@student.unsrat.ac.id>
2023-11-30 17:05:17 +0800

    first commit

---
+++ inc/topBarNav.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-gradient-navy">
+            <div class="container px-4 px-lg-5 ">
+                <button class="navbar-toggler btn btn-sm" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <a class="navbar-brand" href="./">
+                <img src="<?php echo validate_image($_settings->info('logo'))?>" alt="Store Logo" class="brand-image img-circle elevation-3" style="opacity: .8;width: 2rem;height: 2rem;max-height: unset">
+                <?php echo $_settings->info('short_name') ?>
+                </a>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="./">Homepage</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="./?p=reservation">Reservation</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="./?p=tables">Table List</a></li>
+                    </ul>
+                    <div class="d-flex align-items-center">
+                        <a class="font-weight-bolder text-light mx-2 text-decoration-none" href="./admin">Staff Login</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+<script>
+  $(function(){
+    $('#login-btn').click(function(){
+      uni_modal("","login.php")
+    })
+    $('#navbarResponsive').on('show.bs.collapse', function () {
+        $('#mainNav').addClass('navbar-shrink')
+    })
+    $('#navbarResponsive').on('hidden.bs.collapse', function () {
+        if($('body').offset.top == 0)
+          $('#mainNav').removeClass('navbar-shrink')
+    })
+  })
+
+  $('#search-form').submit(function(e){
+    e.preventDefault()
+     var sTxt = $('[name="search"]').val()
+     if(sTxt != '')
+      location.href = './?p=products&search='+sTxt;
+  })
+</script>
---
+++ welcome.php
@@ -0,0 +1,5 @@
+<div style="text-align: center;">
+    <h2 style="margin: 0; padding: 0; font-family: DauphinPlain; font-size: 70px; line-height: 90px;"><span style="font-family: Impact;">Welcome to WarongWarem!</span></h2>
+    <p style="margin: 0; padding: 0;  font-family: Poppins; font-size: 15px; margin-bottom: 5px;">Di WarongWarem, kami bukan sekadar restoran. Kami adalah cerita rasa yang bermula dari keindahan tradisi dan berkembang menjadi inovasi kuliner modern. Setiap hidangan adalah sebuah lukisan rasa, setiap ruangan adalah panggung bagi kenangan yang tak terlupakan.</p>
+    <p style="margin: 0; padding: 0;font-family: Poppins; font-size: 15px; margin-bottom: 15px;">Mari bergabung dalam perjalanan kuliner yang penuh gairah ini, di mana setiap gigitan membawa Anda lebih dekat ke dalam dunia cita rasa yang memukau. Kami dengan bangga menawarkan pengalaman kuliner yang lebih dari sekedar makanan.</p>
+</div>
